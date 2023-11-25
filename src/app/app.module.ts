296d739697d1b42b6046bbae12442b566c624674
Martin Unger <martin-unger2@live.at>
2023-11-25 21:31:01 +0100

    add user + dialog comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,27 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { DialogAddUserComponent } from './dialog-add-user/dialog-add-user.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+
+
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    UserComponent
+    UserComponent,
+    DialogAddUserComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +38,13 @@ import { UserComponent } from './user/user.component';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,   
+    MatDialogModule, 
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
