5e5ea4e440879d84b78e1e4731fe361bc4654e93
Martin Unger <martin-unger2@live.at>
2023-11-27 11:31:38 +0100

    modul user

---
+++ src/models/user.class.ts
@@ -0,0 +1,18 @@
+export class User {
+    firstName: string;
+    lastName: string;
+    birthDate: number;
+    street: string;
+    zipCode: number;
+    city: string;
+
+
+    constructor(obj?: any) {
+        this.firstName = obj ? obj.firstName : '';
+        this.lastName = obj ? obj.lastName : '';
+        this.birthDate = obj ? obj.birthDate : '';
+        this.street = obj ? obj.street : '';
+        this.zipCode = obj ? obj.zipCode : '';
+        this.city = obj ? obj.city : '';
+    }
+}
