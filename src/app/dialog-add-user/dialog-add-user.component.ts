52a325f009fb563db37754eeb705f3095693737e
Martin Unger <martin-unger2@live.at>
2023-12-05 07:28:07 +0100

    ng-model at dialog

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./dialog-add-user.component.scss']
 })
 export class DialogAddUserComponent {
+  user: User = new User;
 
+  saveUser() {
+    console.log('Current user is', this.user);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialog,} from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-user',
@@ -9,7 +10,11 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
 })
 export class UserComponent {
 
-  constructor (public dialog: MatDialog) { }
+  user: User = new User();
+
+  constructor (public dialog: MatDialog) { 
+    this.user
+  }
 
   openDialog() {
     this.dialog.open(DialogAddUserComponent)
